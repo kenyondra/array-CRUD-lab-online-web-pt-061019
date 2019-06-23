c146c62ae1216134a1d93487e12c4a9f3ba3e113
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-23 03:26:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,6 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-<<<<<<< HEAD
   array << element
 end
 
@@ -33,33 +32,6 @@ end
 
 def retrieve_last_element_from_array(array)
   array[-1]
-=======
-  animals_pets = ["dog, "cat", "turtle", "rat"] 
-  animals_pets << "monkey"
-  puts
 end
 
-def add_element_to_start_of_array(array, element)
-  animals_pets.unshift("bird")
-end
 
-def remove_element_from_end_of_array(array)
-  animals_pets.pop
-end
-
-def remove_element_from_start_of_array(array)
-  animals_pets.shift
-end
-
-def retrieve_element_from_index(array, index_number)
-  animals_pets[0]
-end
-
-def retrieve_first_element_from_array(array)
-  animals_pets["dog"]
-end
-
-def retrieve_last_element_from_array(array)
-  animals_pets["rat"]
->>>>>>> 5aa8e4aee0fd153249df3d55e4c0516bbad9c32a
-end
